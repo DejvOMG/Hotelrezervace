ef32cf711331dd2be6972ed387680f8935af3a31
David Srb <david.srb@email.cz>
2024-10-09 12:33:35 +0200

    Initial commit

---
+++ src/Room.java
@@ -0,0 +1,9 @@
+public class Booking {
+    private double room;
+    private int roomNumber;
+    private int bed;
+    private boolean balcony;
+    private boolean seaView;
+    private int price;
+    private 
+}
