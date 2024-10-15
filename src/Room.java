b31f3e902a19642ccce21fd222dc06e6a84ca0b2
DejvOMG <150176972+DejvOMG@users.noreply.github.com>
2024-10-15 10:48:55 +0200

    Room.java

--- src/Room.java
+++ src/Room.java
@@ -1,9 +1,23 @@
-public class Booking {
-    private double room;
+public class Room {
     private int roomNumber;
-    private int bed;
-    private boolean balcony;
-    private boolean seaView;
-    private int price;
-    private 
+    private double pricePerNight;
+    private boolean hasSeaView;
+
+    public Room(int roomNumber, double pricePerNight, boolean hasSeaView) {
+        this.roomNumber = roomNumber;
+        this.pricePerNight = pricePerNight;
+        this.hasSeaView = hasSeaView;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public boolean hasSeaView() {
+        return hasSeaView;
+    }
 }
